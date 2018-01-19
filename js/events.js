9d45ed6573e400ad654479137303491ac8ae8b33
elijahmm17@gmail.com <elijahmm17@gmail.com>
2018-01-19 14:49:41 +0000

    automatically backed up by learn

--- js/events.js
+++ js/events.js
@@ -1,7 +1,26 @@
 //define functions here
 
+function getIt() {
+  $('p').on('click', function() {
+    alert("Hey!");
+  });
+}
+
+function frameIt() {
+  $('img').on('load', function() {
+    $('img').addClass('tasty');
+  });
+}
+
+function pressIt() {
+  $('input').on('keydown', function() {
+    if ()
+  })
+}
+
 $(document).ready(function(){
 
 // call functions here
 
 });
+
